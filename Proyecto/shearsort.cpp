9caded95f9823a925bb8415df78a3c1f18789a40
Pablo <palca2093@gmail.com>
2016-10-18 22:42:21 -0600

    Avance de proyecto

--- Proyecto/shearsort.cpp
+++ Proyecto/shearsort.cpp
@@ -21,32 +21,32 @@ Shearsort::~Shearsort()
 void sort(){
 
 
-	Shearsort m1(this->n, this->matrix);
-	Shearsort sort(n,this->matrix);
+	Shearsort m1(this->n, this->matriz);
+	Shearsort sort(n,this->matriz);
 
 	int contador;
 		while(contador!=(n+1)){
 
 			for(int x=0; x<n; x++){
-				
-				if((n%2)==0){//filas pares
-					for(int i=0; i<n; i++){
-						for(int j=0; j<n; j++){
-							if(matriz[i][j]<matriz[i][j+1]){
-								int temp=matriz[i][j];
-								matriz[i][j]=matriz[i][j+1];
-								matriz[i][j+1]=temp;
+
+				if((this->n%2)==0){//filas pares
+					for(int i=0; i<this->n; i++){
+						for(int j=0; j<this->n; j++){
+							if(this->matriz[i][j]<this->matriz[i][j+1]){
+								int temp=this->matriz[i][j];
+								this->matriz[i][j]=this->matriz[i][j+1];
+								this->matriz[i][j+1]=temp;
 	      }else{}
 	     }
 	    }
 	   }
-				else if((n%2)!=0){//filas impares
-					for(int i=0; i<n; i++){
-						for(int j=0; j<n; j++){
-							if(matriz[i][j]>matriz[i][j+1]){
-								int temp=matriz[i][j];
-								matriz[i][j]=matriz[i][j+1];
-								matriz[i][j+1]=temp;
+				else if((this->n%2)!=0){//filas impares
+					for(int i=0; i<this->n; i++){
+						for(int j=0; j<this->n; j++){
+							if(this->matriz[i][j]>this->matriz[i][j+1]){
+								int temp=this->matriz[i][j];
+								this->matriz[i][j]=this->matriz[i][j+1];
+								this->matriz[i][j+1]=temp;
 	      }else{}
 	     }
 	    }
@@ -59,8 +59,8 @@ void sort(){
 
 void Shearsort::operator~() {
 
-	for(int i=0;i<m;i++){
-			for(int j=0; j<n; j++){
+	for(int i=0;i<this->n;i++){
+			for(int j=0; j<this->n; j++){
 				cout<< this->matriz[i][j]<<"\t";
 			}
 			cout<<endl;
